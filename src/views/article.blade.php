5e023eb7c9eda13876cc9e2f7a494738ab84b906
wangke <wangke85@gmail.com>
2014-09-30 01:30:28 +0800

    article临时加入了一个uploadify

--- src/views/article.blade.php
+++ src/views/article.blade.php
@@ -6,6 +6,10 @@
     {{{ $field->value() }}}
     </textarea>
     </div>
+    <div class="row">
+        <input type="text" class="uploadify-result form-control" readonly>
+        <input type="file" id="{{ '_uploadify-text-' . sha1( sprintf( "%08d" , rand( 0 , 99999999 ) ) . microtime() ) }}" form-role="image-upload-richtext" uploadify-swf-path="{{ KUrl::asset( 'uploadify/uploadify.swf' , null , false ) }}" uploadify-upload-url="{{ URL::action( 'admin.uploadify.image' ) }}">
+    </div>
     <div class="text-danger">
     </div>
 </div>
