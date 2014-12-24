8f5f274e2038306329ea5ae1917b136fd978a641
wangke <wangke85@gmail.com>
2014-12-24 18:48:39 +0800

    删除图片

--- src/views/image.blade.php
+++ src/views/image.blade.php
@@ -13,10 +13,12 @@
  <div {{ $field->formgroup() }}>
     <label for="{{ $_name }}" class="control-label">{{ $field->label() }}</label>
     <div class="row">
-        <div form-role="image-upload" uploadify-image-type="{{{ $field->type() }}}" uploadify-swf-path="{{ \KUrl::asset( 'uploadify/uploadify.swf' , null , false ) }}" uploadify-upload-url="{{ \URL::action( 'uploadify.image' ) }}">
+        <div class="div-image-upload" form-role="image-upload" uploadify-image-type="{{{ $field->type() }}}" uploadify-swf-path="{{ \KUrl::asset( 'uploadify/uploadify.swf' , null , false ) }}" uploadify-upload-url="{{ \URL::action( 'uploadify.image' ) }}">
             <img src="{{ $field->value() }}" class="img-responsive uploadify-image">
             <input hidden="hidden" class="hidden uploadify-input" name="{{ $field->name() }}" type="text" value="{{ $field->value() }}">
             <input id="_uploadify-image-{{ $_name }}" name="_uploadify" type="file">
+            <a href="javascript:;" class="uploadify-remove-image"
+               onclick="$(this).parents('div.div-image-upload').childrren('img').attr('src' , '');$(this).parents('div.div-image-upload').childrren('.uploadify-input').attr('value' , '');">x</a>
         </div>
     </div>
      <div class="text-danger">
